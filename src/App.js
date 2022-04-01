bc160481a03006e37f1135db1d9ca466b6022dde
mahbubur1 <mdmahbuborr462@gail.com>
2022-04-01 23:48:47 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Countries from './Components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
     </div>
   );
 }
 
+/* function LoadCountriesData (){
+  const [countries,setCountries] = useState([])
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  } ,[])
+  return (
+    <div>
+      <h1>All Countries Data</h1>
+      <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country =><Country name={country.name.common}></Country>)
+      }
+    </div>
+  )
+} */
+/* function Country (props){
+  return (
+    <div>
+      <h3>Name: {props.name}</h3>
+    </div>
+  )
+} */
 export default App;
